227dc36617f2a35b93dd4990ca1e91e583d2f6ed
gevbusiness <ggtherrappack@yahoo.com>
2023-04-09 16:13:41 +0400

    individual git push

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# spin-the-globe1
+# spin-the-globe
+
+## Description
+SPIN THE GLOBE provides users a random travel destination generator, which includes the top tourist attractions and correspondig hotels. The webpage helps users pick thier next destination in case they do not know where to travel next. 
+
+SPIN THE GLOBE solves the problem of not being able to guess where to travel next. It provides the end user a random destination if the user does not have the capability to select their next destination. 
+
+As a group, we learned how to navigate through attained information provided by different WEB API's. We learned how to output the needed information out of the API, and extract the result of the first API into the second API. 
+
+
+## Usage
+
+When the browser opens, there are three columns displaying on the screen. The middle column is where we have our interactive element of the application. When the user clicks on the globe image displayed, they will bw given a random country at the top of the page, a list of toursit attractions on the left column, and if available, a list of hotels for them to stay at. If the user leaves the page and comes back, there will be a message displaying their last "destination", and this will update every time they click on the globe image.
+
+![Webpage before the user clicks](./assets/images/Screenshot%202023-04-06%20at%209.54.57%20PM.png)
+
+![Webpage after the user clicks](./assets/images/127.0.0.1_5502_index.html.png)
+
+
+## Credits
+
+List of collaborators:
+
+-Gevorg Avetisyan (GitHub: gevbusiness)
+-Haojie Hu ( GitHub: Justinhu97)
+-Joseph Moreland (GitHub: mosjoreland)
+-Laura Mendez (GitHub: lim204)
+-Philip Mejia (GitHub: RepoPanda)
+
+List of Web API's:
+- GeoApify: a feature-rich location platform suitable for businesses of any size used for finding destination recomended destination within the  randomly selected country.
+- Hotels.com API: helped to query rooms, price, facilities, policities, etc information from many hotels around the world.
+
+## Badges
+
+NA
+
+## Features
+
+TOP TOURIST DESTINATIONS
+GOOGLE SEARCH OF THE DESTINATION
+HOTELS OF THE DESTINATION
+
---
+++ assets/java script/script.js
@@ -0,0 +1,366 @@
+// add El to the js file
+var introEl = document.getElementById("intro");
+var linkEl = document.getElementById("link");
+var nameEl = document.getElementById("nameofrecommand");
+var addressEl = document.getElementById("addressofrecommand");
+var globeImageEl = document.getElementById("img");
+var subTitleEl = document.querySelector("h4");
+var containerEl = document.getElementById("container");
+var hotelcontainerEl = document.getElementById("hotelcontainer");
+var footerEl = document.querySelector("footer");
+
+// event listener to make the intro disappear
+introEl.addEventListener("click", function () {
+  introEl.innerHTML = " ";
+});
+
+// array of the countries
+const countryList = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bonaire, Sint Eustatius and Saba",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Bouvet Island",
+  "Brazil",
+  "British Indian Ocean Territory",
+  "Brunei Darussalam",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cape Verde",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Cayman Islands",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Curacao",
+  "Cyprus",
+  "Czechia",
+  "Cote d'Ivoire",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Guiana",
+  "French Southern Territories",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guadeloupe",
+  "Guam",
+  "Guatemala",
+  "Guernsey",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Heard Island and McDonald Islands",
+  "Holy See",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jersey",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Lao People's Democratic Republic",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Martinique",
+  "Mauritania",
+  "Mauritius",
+  "Mayotte",
+  "Mexico",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Nigeria",
+  "Niue",
+  "Norfolk Island",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine, State of",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Pitcairn",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Republic of North Macedonia",
+  "Romania",
+  "Russian Federation",
+  "Rwanda",
+  "Saint Barthelemy",
+  "Saint Helena, Ascension and Tristan da Cunha",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Pierre and Miquelon",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Georgia and the South Sandwich Islands",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Svalbard and Jan Mayen",
+  "Sweden",
+  "Switzerland",
+  "Syrian Arab Republic",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania, United Republic of",
+  "Thailand",
+  "Togo",
+  "Tokelau",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Turks and Caicos Islands",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Kingdom of Great Britain and Northern Ireland",
+  "United States of America",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Viet Nam",
+  "Wallis and Futuna",
+  "Western Sahara",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+  "Aland Islands",
+];
+
+// local storage
+var lastResult = localStorage.getItem("lastResult");
+footerEl.innerHTML = "Your last result is: " + lastResult;
+
+// outputs a random country
+globeImageEl.addEventListener("click", function () {
+  var randomCountryIndex = Math.floor(Math.random() * countryList.length);
+  var randomCountry = countryList[randomCountryIndex];
+  console.log(randomCountry);
+  var placeId;
+  subTitleEl.innerHTML = "Let's travel to " + randomCountry + "!";
+  linkEl.innerHTML = "https://www.google.com/search?q=" + randomCountry;
+  linkEl.setAttribute(
+    "href",
+    "https://www.google.com/search?q=" + randomCountry
+  );
+
+  // first api to grab geoID for second api
+  fetch(
+    `https://api.geoapify.com/v1/geocode/search?text=${randomCountry}&format=json&apiKey=5105f0bb5b7c4e97bceb5e70a9de8ceb`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      for (let index = 0; index < data.results.length; index++) {
+        if (randomCountry === data.results[index].country) {
+          placeId = data.results[index].place_id;
+        }
+      }
+
+      // second api will provide recommended destinations
+      fetch(
+        `https://api.geoapify.com/v2/places?categories=tourism.attraction&filter=place:${placeId}&apiKey=5105f0bb5b7c4e97bceb5e70a9de8ceb`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+
+          // render information back to our web page
+          let index = 0;
+          nameEl.innerHTML = data.features[index].properties.address_line1;
+          addressEl.innerHTML = data.features[index].properties.address_line2;
+
+          while (index < data.features.length - 1) {
+            index++;
+
+            var newnameEl = document.createElement("li");
+            var newaddressEl = document.createElement("li");
+            var newBrEl = document.createElement("br");
+
+            containerEl.append(newnameEl);
+            containerEl.append(newaddressEl);
+            containerEl.append(newBrEl);
+
+            newnameEl.textContent =
+              data.features[index].properties.address_line1;
+            newaddressEl.innerHTML =
+              data.features[index].properties.address_line2;
+          }
+
+          // third api will provide hotel information
+          const options = {
+            method: "GET",
+            headers: {
+              "X-RapidAPI-Key":
+                "e85ea256d0mshf1ec8dba09eaac2p196d46jsn8cc5e90c6f6f",
+              "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+            },
+          };
+
+          fetch(
+            `https://hotels4.p.rapidapi.com/locations/v3/search?q=${randomCountry}&locale=en_US&langid=1033&siteid=300000001`,
+            options
+          )
+            .then((response) => response.json())
+            .then(function (data) {
+              console.log(data);
+
+              // render information back to our web page
+              var count = 0;
+              for (let index = 0; index < data.sr.length; index++) {
+                if (data.sr[index].type === "HOTEL") {
+                  
+                  // here is the information show on the page
+                  var newhotelnameEl = document.createElement("li");
+                  var newhoteladdressEl = document.createElement("li");
+                  var newBrEl = document.createElement("br");
+
+                  hotelcontainerEl.append(newhotelnameEl);
+                  hotelcontainerEl.append(newhoteladdressEl);
+                  hotelcontainerEl.append(newBrEl);
+
+                  newhotelnameEl.textContent =
+                    data.sr[index].regionNames.fullName;
+                  newhoteladdressEl.innerHTML =
+                    data.sr[index].hotelAddress.street;
+                } else if (data.sr[index].type !== "HOTEL") {
+                  count++;
+                }
+              }
+              
+              // check for if there is any hotel information showed
+              if (count === data.sr.length) {
+                var hotelwarningEl = document.createElement("li");
+
+                hotelcontainerEl.append(hotelwarningEl);
+
+                hotelwarningEl.innerHTML =
+                  "Sorry, the Hotel Recommendation services is not avaliable!";
+              }
+
+              // local storage to save last result
+              localStorage.setItem("lastResult", randomCountry);
+            });
+        });
+    });
+});
+
